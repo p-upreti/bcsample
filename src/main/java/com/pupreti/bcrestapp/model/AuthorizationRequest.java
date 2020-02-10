caafdac2c6b34b0f157cdb5cd15f987970574620
p-upreti <pupreti1@gmail.com>
2020-02-09 22:44:25 -0500

    some validation-need more

--- src/main/java/com/pupreti/bcrestapp/model/AuthorizationRequest.java
+++ src/main/java/com/pupreti/bcrestapp/model/AuthorizationRequest.java
@@ -1,10 +1,14 @@
 package com.pupreti.bcrestapp.model;
 
 import java.util.List;
+import javax.validation.constraints.Size;
 
 public class AuthorizationRequest {
 
+    @Size(min=1)
     private List<String> accountIds;
+
+    @Size(min=1, max=3)
     private List<Operation> operations;
 
     public List<String> getAccountIds() {
